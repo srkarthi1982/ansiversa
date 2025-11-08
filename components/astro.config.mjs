99e22fcba3724d2de7e3e516d3a527f8b112230d
InfiniteMediaStudio <infinitemedia92@gmail.com>
2025-11-08 20:09:15 +0400

    components folder added

---
+++ components/astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    vite: {
+        logLevel: 'error', // hides Vite warnings
+    },
+});
